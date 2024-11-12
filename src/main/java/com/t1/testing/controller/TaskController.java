de3f33f3c2c0bb5182de07bb1885776a339bdf95
Кирилл Максимович <kirillking01@yandex.ru>
2024-11-12 04:48:43 +0300

    fix: add @RequestBody annotations in controller

--- src/main/java/com/t1/testing/controller/TaskController.java
+++ src/main/java/com/t1/testing/controller/TaskController.java
@@ -8,23 +8,22 @@ import com.t1.testing.service.sample.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/task")
 @RequiredArgsConstructor
-@Validated
 public class TaskController {
 
     private final TaskMapper taskMapper;
@@ -32,7 +31,7 @@ public class TaskController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public TaskDto createTask(@Valid TaskCreateDto dto) {
+    public TaskDto createTask(@RequestBody @Valid TaskCreateDto dto) {
         return taskMapper.taskToDto(taskService.createTask(taskMapper.taskCreateDtoToTask(dto)));
     }
 
@@ -46,21 +45,21 @@ public class TaskController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TaskDto getTaskById(@PathVariable Long taskId) {
-        return taskMapper.taskToDto(taskService.getTaskById(taskId));
+    public TaskDto getTaskById(@PathVariable Long id) {
+        return taskMapper.taskToDto(taskService.getTaskById(id));
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TaskDto updateTaskById(@PathVariable Long taskId,
-                                  @Valid TaskUpdateDto dto) {
-        return taskMapper.taskToDto(taskService.updateTaskById(taskId, taskMapper.taskUpdateDtoToTask(dto)));
+    public TaskDto updateTaskById(@PathVariable Long id,
+                                  @Valid @RequestBody TaskUpdateDto dto) {
+        return taskMapper.taskToDto(taskService.updateTaskById(id, taskMapper.taskUpdateDtoToTask(dto)));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteTaskById(@PathVariable Long taskId) {
-        taskService.deleteTaskById(taskId);
+    public void deleteTaskById(@PathVariable Long id) {
+        taskService.deleteTaskById(id);
     }
 
 }
