ce64901608800f82f9837ec77626cc3b0935a5af
Кирилл Максимович <kirillking01@yandex.ru>
2024-11-12 04:52:33 +0300

    fix: add NoArgsConstructor in Task entity

--- src/main/java/com/t1/testing/entity/Task.java
+++ src/main/java/com/t1/testing/entity/Task.java
@@ -10,10 +10,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
